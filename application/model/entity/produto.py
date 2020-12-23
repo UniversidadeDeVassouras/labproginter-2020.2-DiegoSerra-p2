4eeeb8c7a2d100161750d95a109a4611d9e89e76
diego serra <diegoserra120@hotmail.com>
2020-12-23 11:05:44 -0300

    inserindo projeto

--- application/model/entity/produto.py
+++ application/model/entity/produto.py
@@ -9,27 +9,33 @@ class Produto:
         self._parcela = parcela
         self._valor_parcela = valor_parcela
 
+    def get_id(self):
+        return self._id
+
 
     def get_nome(self):
         return self._nome
 
-    def get_id(self):
-        return self._id
 
     def get_imagem(self):
         return self._imagem
 
+
     def get_preco_antes(self):
         return self._preco_antes
+
 
     def get_preco(self):
         return self._preco
 
+
     def get_descricao(self):
         return self._descricao
 
+
     def get_parcela(self):
         return self._parcela
+        
 
     def get_valor_parcela(self):
         return self._valor_parcela
